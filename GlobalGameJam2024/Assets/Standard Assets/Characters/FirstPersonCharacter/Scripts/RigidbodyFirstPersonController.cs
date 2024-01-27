464ca9e51cfeeb791fa5bca52563df3c7c7d88ae
Rimme <rimme@happyvolcano.com>
2024-01-27 15:14:16 +0100

    New player prefab

--- GlobalGameJam2024/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ GlobalGameJam2024/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -142,25 +142,32 @@ namespace UnityStandardAssets.Characters.FirstPerson
             GroundCheck();
             Vector2 input = GetInput();
 
-            if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
-            {
-                // always move along the camera forward as it is the direction that it being aimed at
-                Vector3 desiredMove = cam.transform.forward*input.y + cam.transform.right*input.x;
-                desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
+            //if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
+            //{
+            //    // always move along the camera forward as it is the direction that it being aimed at
+            //    Vector3 desiredMove = cam.transform.forward*input.y + cam.transform.right*input.x;
+            //    desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
 
-                desiredMove.x = desiredMove.x*movementSettings.CurrentTargetSpeed;
-                desiredMove.z = desiredMove.z*movementSettings.CurrentTargetSpeed;
-                desiredMove.y = desiredMove.y*movementSettings.CurrentTargetSpeed;
-                if (m_RigidBody.velocity.sqrMagnitude <
-                    (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
-                {
-                    m_RigidBody.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
-                }
-            }
+            //    desiredMove.x = desiredMove.x*movementSettings.CurrentTargetSpeed;
+            //    desiredMove.z = desiredMove.z*movementSettings.CurrentTargetSpeed;
+            //    desiredMove.y = desiredMove.y*movementSettings.CurrentTargetSpeed;
+            //    //if (m_RigidBody.velocity.sqrMagnitude <
+            //    //    (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
+            //    //{
+            //    //    m_RigidBody.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
+            //    //}
+            //}
+            Vector3 desiredMove = cam.transform.forward * input.y + cam.transform.right * input.x;
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
+
+            desiredMove.x = desiredMove.x * movementSettings.CurrentTargetSpeed;
+            desiredMove.z = desiredMove.z * movementSettings.CurrentTargetSpeed;
+            desiredMove.y = desiredMove.y * movementSettings.CurrentTargetSpeed;
+            m_RigidBody.velocity = Vector3.Lerp(m_RigidBody.velocity, new Vector3(desiredMove.x, m_RigidBody.velocity.y, desiredMove.z), Time.deltaTime * 100.0f);
 
             if (m_IsGrounded)
             {
-                m_RigidBody.drag = 5f;
+                m_RigidBody.drag = 10f;
 
                 if (m_Jump)
                 {
